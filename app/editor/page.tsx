1c33741c8daccc7d7e024dcb797bf4f94b2a3082
Gilbert Young Jr <gilbertjyoungjr@gmail.com>
2024-01-03 20:46:58 -0600

    fix styles on editor layout

--- app/editor/page.tsx
+++ app/editor/page.tsx
@@ -1,5 +1,38 @@
+'use client';
 import React from 'react';
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from '@/components/ui/resizable';
 
-export default function CodeEditor() {
-  return <div>Code Editor</div>;
+function ResizableDemo() {
+  return (
+    <div
+      style={{
+        height: 'calc(100vh - 4rem)',
+      }}
+    >
+      <ResizablePanelGroup direction="horizontal" className="w-full  border">
+        <ResizablePanel defaultSize={10}>
+          <div className="flex h-full items-center justify-center p-6">
+            <span className="font-semibold">file</span>
+          </div>
+        </ResizablePanel>
+        <ResizableHandle withHandle />
+        <ResizablePanel defaultSize={65}>
+          <div className="flex h-full items-center justify-center p-6">
+            <span className="font-semibold">IDE</span>
+          </div>
+        </ResizablePanel>
+        <ResizableHandle withHandle />
+        <ResizablePanel defaultSize={25}>
+          <div className="flex h-full items-center justify-center p-6">
+            <span className="font-semibold">ChatBot</span>
+          </div>
+        </ResizablePanel>
+      </ResizablePanelGroup>
+    </div>
+  );
 }
+export default ResizableDemo;
